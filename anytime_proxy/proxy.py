a6af27c133b6048b909fa4e02bed2d033cd22239
Xiaoyang Liu <siujoeng.lau@gmail.com>
2020-05-30 11:49:40 +0800

    Create from_dict classmethod and anonymity_level, host, relay properties for BaseProxy.
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- anytime_proxy/proxy.py
+++ anytime_proxy/proxy.py
@@ -6,9 +6,9 @@ class BaseProxy:
             2: "High Anonymous"
         }
         self._valid_protocols = {
-            "http", 
-            "https", 
-            "socks5", 
+            "http",
+            "https",
+            "socks5",
             "socks4"
         }
         self._geo_info = {
@@ -18,12 +18,15 @@ class BaseProxy:
             "region_name": "",
             "city_name": ""
         }
+
         self._protocol = protocol
         self._host = host
         self._port = port
+        self._relay = False
         self._anonymity = 0
         self._test_times = 0
         self._average_latency = 0.0
+        self._validity = 100
 
     def __str__(self):
         return self.__repr__()
@@ -33,7 +36,47 @@ class BaseProxy:
             protocol=self._protocol,
             host=self._host,
             port=self._port,
-            geo_code=self._geo_code
-            anonymty=self._anonymity_level[self._anonymity]
+            geo_code=self._geo_info['code'],
+            anonymity=self._anonymity_level[self._anonymity]
         )
-    
+
+    @classmethod
+    def from_dict(cls, d):
+        df = {'_' + k: v for k, v in d.items()}
+        return cls(**df)
+
+    def to_dict(self):
+        return {
+            "geo_info": self._geo_info,
+            "protocol": self._protocol,
+            "host": self._host,
+            "port": self._port,
+            "relay": self.relay,
+            "anonymity": self.anonymity_level,
+            "average_latency": self._average_latency,
+            "test_times": self._test_times,
+            "validity": self._validity
+        }
+
+    @property
+    def host(self):
+        return self._host
+
+    @property
+    def relay(self):
+        return self._relay
+
+    @relay.setter
+    def relay(self, is_relay):
+        if isinstance(is_relay, bool):
+            self._relay = is_relay
+
+    @property
+    def anonymity_level(self):
+        return self._anonymity_level
+
+    @anonymity_level.setter
+    def anonymity_level(self, level):
+        if not level == 1 or level == 2:
+            level = 0
+        self._anonymity_level = level
