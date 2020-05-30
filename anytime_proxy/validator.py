f97c6dad27636f84a106799348ff7d6d985e5e07
Xiaoyang Liu <siujoeng.lau@gmail.com>
2020-05-30 11:50:30 +0800

    Create get_real_ip method to obtain the real IP of the device in order to  check the anonymity level.
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- anytime_proxy/validator.py
+++ anytime_proxy/validator.py
@@ -1,22 +1,45 @@
 import aiohttp
 import asyncio
 import anytime_proxy as ap
-import logger
-
+import logging
 
 
 class Validator:
-    # 
     def __init__(self, proxy_list):
         test_urls = {
             'https://httpbin.org/get?show_env'
         }
+        self.real_ip = "127.0.0.1"
+        asyncio.run(
+            self.get_real_ip()
+        )
         asyncio.run(
             self.test_bulk(
-                proxy_list, 
+                proxy_list,
                 test_urls
             )
         )
+
+    async def get_real_ip(self) -> None:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://httpbin.org/get', timeout=15) as resp:
+                    resp_json = await resp.json()
+        except (
+                aiohttp.ClientError,
+                aiohttp.ClientConnectorError,
+                TimeoutError,
+                AttributeError
+        ) as exc:
+            logging.error(
+                "aiohttp exception for {} [{}]: {}".format(
+                    'https://httpbin.org/get',
+                    getattr(exc, "status", None),
+                    getattr(exc, "message", None)
+                ),
+            )
+        else:
+            self.real_ip = resp_json.get("origin", self.real_ip)
 
     async def test_bulk(self, proxy_list: list, test_urls: set) -> None:
         async with aiohttp.ClientSession() as session:
@@ -24,25 +47,23 @@ class Validator:
             for proxy in proxy_list:
                 for url in test_urls:
                     tasks.append(
-                        self.fetch_html(proxy, url)
+                        self.fetch_html(proxy, url, session)
                     )
             await asyncio.gather(*tasks)
 
     async def fetch_html(self, proxy: ap.BaseProxy, url: str, session: aiohttp.ClientSession) -> None:
-        found = set()
         try:
-            async with session.get('http://httpbin.org/get', proxy=proxy, params=params) as resp:
-                logger.info("Got response [%s] for URL: %s", resp.status, url)
+            async with session.get(url, proxy=proxy, timeout=15) as resp:
+                logging.info("Got response [%s] for URL: %s", resp.status, url)
                 resp.raise_for_status()
                 resp_json = await resp.json()
         except (
-            aiohttp.ClientError,
-            aiohttp.ClientConnectorError,
-            aiohttp.http_exceptions.HttpProcessingError,
-            TimeoutError, 
-            AttributeError
+                aiohttp.ClientError,
+                aiohttp.ClientConnectorError,
+                TimeoutError,
+                AttributeError
         ) as exc:
-            logger.error(
+            logging.error(
                 "aiohttp exception for {} [{}]: {}".format(
                     url,
                     getattr(exc, "status", None),
@@ -50,8 +71,8 @@ class Validator:
                 ),
             )
         except Exception as e:
-            logger.exception(
-                "Non-aiohttp exception occured:  {}".format(
+            logging.error(
+                "Non-aiohttp exception occured: {}".format(
                     getattr(e, "__dict__", {})
                 )
             )
@@ -70,6 +91,6 @@ class Validator:
                 proxy.anonymity_level = 2
             else:
                 proxy.anonymity_level = 1
-            logger.info("Vaild Proxy: {}".format(proxy))
+            logging.info("Valid Proxy: {}".format(proxy))
         finally:
             await session.close()
